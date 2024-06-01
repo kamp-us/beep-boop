2927d2ccffa3a4998993b33bb660ad7ace51b4f7
usirin <umutsirin1@gmail.com>
2024-06-01 12:33:53 -0700

    [llm-bot] initial commit

---
+++ bots/llm-bot/commands.ts
@@ -0,0 +1,18 @@
+import { type ChatInputCommandInteraction, GuildScheduledEventManager } from "discord.js";
+
+export type CommandCollection = Record<
+	string,
+	{
+		description: string;
+		handler: (interaction: ChatInputCommandInteraction) => Promise<void>;
+	}
+>;
+
+export const commands = {
+	healthcheck: {
+		description: "Check if the bot is healthy.",
+		async handler(interaction) {
+			await interaction.reply("I'm healthy!");
+		},
+	},
+} satisfies CommandCollection;
